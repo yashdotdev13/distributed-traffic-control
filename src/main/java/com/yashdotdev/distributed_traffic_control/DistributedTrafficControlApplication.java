b76cbaa99fc0c3a1adf7d16c08c9882674cb1d04
98001yash <yashchauhan.gaya@gmail.com>
2026-08-27 23:42:46 +0530

    setup done

---
+++ src/main/java/com/yashdotdev/distributed_traffic_control/DistributedTrafficControlApplication.java
@@ -0,0 +1,13 @@
+package com.yashdotdev.distributed_traffic_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTrafficControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTrafficControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yashdotdev/distributed_traffic_control/DistributedTrafficControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashdotdev.distributed_traffic_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTrafficControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
